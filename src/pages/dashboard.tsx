07c240dc917a3b69cc93c8eb85cfa09a1f74d8af
Rodrigo Correa de Godoy Camargo <rodrigo.godoy@hbsis.com.br>
2022-08-28 18:34:07 -0300

    (Header)Added icon search

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,5 @@
+import { Header } from "./components/Form/Header";
+
+export default function Dashboard() {
+  return <Header/>;
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,7 @@
 import React from "react";
 
-import {
-  Flex,
-  Input,
-  Button,
-  Stack,
-  FormLabel,
-  FormControl,
-} from "@chakra-ui/react";
+import { Flex, Button, Stack } from "@chakra-ui/react";
+import { Input } from "./components/Form/Input";
 
 export default function Home() {
   return (
@@ -22,40 +16,8 @@ export default function Home() {
         flexDir="column"
       >
         <Stack spacing="4">
-          <FormControl>
-            <FormLabel htmlFor="email">Email</FormLabel>
-            <Input
-              id="email"
-              color="white"
-              width=""
-              name="email"
-              type="email"
-              focusBorderColor="pink.500"
-              bgColor="gray.900"
-              variant="filled"
-              _hover={{
-                bgColor: "gray.900",
-              }}
-              size="lg"
-            />
-          </FormControl>
-          <FormControl>
-            <FormLabel htmlFor="password">Password</FormLabel>
-            <Input
-              id="password"
-              color="white"
-              width=""
-              name="password"
-              type="password"
-              focusBorderColor="pink.500"
-              bgColor="gray.900"
-              variant="filled"
-              _hover={{
-                bgColor: "gray.900",
-              }}
-              size="lg"
-            />
-          </FormControl>
+          <Input name="email" type="email" label="E-mail" />
+          <Input name="password" type="password" label="Password" />
         </Stack>
 
         <Button type="submit" mt="6" colorScheme="pink">
