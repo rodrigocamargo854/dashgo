a9aef3bf8882f91abd016954bb36cc3233906110
Rodrigo Correa de Godoy Camargo <rodrigo.godoy@hbsis.com.br>
2022-08-28 15:04:07 -0300

    theme colors ok

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,7 +5,7 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "./styles/theme";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider resetCSS theme={theme}>
+    <ChakraProvider resetCSS = {false} theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
   )
--- src/pages/styles/theme.ts
+++ src/pages/styles/theme.ts
@@ -1,12 +1,26 @@
 import { extendTheme } from "@chakra-ui/react";
 
-export const theme = extendTheme ({
-    styles:{
-        global: {
-            body:{
-                bg:'gray.900',
-                color:'gray.50'
-            }
-        }
+export const theme = extendTheme({
+  colors: {
+    gray: {
+        "50":"#EEEEF2",
+        "100":"#D1D2DC",
+        "200":"#B3B5C6",
+        "300":"#9699B0",
+        "400":"#797D9A",
+        "500":"#616488",
+        "600":"#4B4D63",
+        "700":"#353646",
+        "800":"#1F2029",
+        "900":"#181B23",
     }
-})
+  },
+  styles: {
+    global: {
+      body: {
+        bg: "gray.900",
+        color: "gray.50",
+      },
+    },
+  },
+});
