493cfcbe6c731469a70ec694e82308b479a2be6f
Rodrigo Correa de Godoy Camargo <rodrigo.godoy@hbsis.com.br>
2022-09-10 09:12:11 -0300

    created component for SideBar

---
+++ src/pages/components/SideBar/NavSection.tsx
@@ -0,0 +1,23 @@
+import { Box, Stack, Text } from "@chakra-ui/react";
+import { ReactNode } from "react";
+
+
+
+interface NavSectionProps {
+    title: string;
+    children: ReactNode
+}
+
+export function NavSection({ title, children }: NavSectionProps) {
+    return (
+        <Box>
+            <Text fontWeight="bold" color="gray.400" fontSize="small">
+                {title}
+            </Text>
+            <Stack spacing="1" mt="8" align="stretch">
+               {children}
+            </Stack>
+        </Box>
+    );
+}
+
