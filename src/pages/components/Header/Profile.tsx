21f441cc3301d81b8930178d43268acb67f12044
Rodrigo Correa de Godoy Camargo <rodrigo.godoy@hbsis.com.br>
2022-09-10 14:03:19 -0300

    added logic to visibility to Hader

--- src/pages/components/Header/Profile.tsx
+++ src/pages/components/Header/Profile.tsx
@@ -1,18 +1,20 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+    showProfileData?: boolean;
+}
+export function Profile({ showProfileData = true }: ProfileProps) {
     return (
         <Flex align="center" >
-            <Box mr="4" textAlign="right">
+            {showProfileData && <Box mr="4" textAlign="right">
                 <Text>
                     Rodrigo Camargo
                 </Text>
                 <Text color="gray.300" fontSize="small">
                     rodrigocamargo854@gmail.com
                 </Text>
-            </Box>
+            </Box>}
             <Avatar size="md" name="Rodrigo Camargo" src="https://github.com/rodrigocamargo" />
         </Flex>
-
     );
 }
