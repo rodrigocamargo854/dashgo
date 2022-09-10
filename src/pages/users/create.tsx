1464ba7753dbf6ae12d54b5d5d6162b4a01ed9ca
Rodrigo Correa de Godoy Camargo <rodrigo.godoy@hbsis.com.br>
2022-09-10 07:30:24 -0300

    fixed all import for new components paths

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -1,9 +1,16 @@
 import {
     Box,
+    Divider,
     Flex,
-   
+    Heading,
+    HStack,
+    SimpleGrid,
+    VStack,
+    Button
+
 } from "@chakra-ui/react";
-import { Header } from "../components/Form/Header";
+import { Header } from "../components/Header/index";
+import { Input } from "../components/Form/Input";
 import SideBar from "../components/Form/SideBar";
 
 export default function CreateUser() {
@@ -13,7 +20,30 @@ export default function CreateUser() {
             <Flex w="100%" my="6" maxWidth={1400} mx="auto" px="6">
                 <SideBar />
                 <Box flex="1" borderRadius="8" bg="gray.800" p="8">
-                   
+                    <Heading size="lg" fontWeight="normal">Criar Usuário</Heading>
+
+                    <Divider my="6" borderColor="gray.700" />
+                    <VStack spacing="8">
+                        <SimpleGrid minChildWidth="248px" spacing="8" w="100%">
+                            <Input name="name" label="nome completo"></Input>
+                            <Input name="email" type="email" label="E-mail"></Input>
+                        </SimpleGrid>
+
+                        <SimpleGrid minChildWidth="248px" spacing="8" w="100%">
+                            <Input name="password" label="password"></Input>
+                            <Input name="password_confirmation" type=" password" label="confirmação da senha"></Input>
+                        </SimpleGrid>
+                    </VStack>
+                    <Flex mt='8' justify='flex-end' >
+                        <HStack spacing='4'>
+                            <Button colorScheme="whiteAlpha">
+                                Cancelar
+                            </Button>
+                            <Button colorScheme="pink">
+                                Salvar
+                            </Button>
+                        </HStack>
+                    </Flex>
                 </Box>
             </Flex>
         </Box>
