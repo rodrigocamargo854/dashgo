f3125676f9652752b4a6adb175a41d70eaf9e621
Rodrigo Correa de Godoy Camargo <rodrigo.godoy@hbsis.com.br>
2022-09-10 14:03:36 -0300

    added component do SideBar

--- src/pages/components/SideBar/index.tsx
+++ src/pages/components/SideBar/index.tsx
@@ -1,29 +1,34 @@
-import { Box, Stack, Text, Link, Icon } from "@chakra-ui/react";
-import { RiContactsLine, RiDashboardLine, RiInputMethodLine } from "react-icons/ri";
-import { NavLink } from "./NavLink";
-import { NavSection } from "./NavSection";
+import { Box, useBreakpointValue, Drawer, DrawerOverlay, DrawerCloseButton, DrawerBody, DrawerContent, DrawerHeader } from "@chakra-ui/react";
+import { SideNavBar } from "./SideNavBar";
 
 export default function SideBar() {
+// para crair uma responsividade sobreposta do sidebar tpo draawer
+    const isDrawSideBar = useBreakpointValue({
+        base: true,
+        lg: false
+    })
+
+    if (isDrawSideBar) {
+        return (
+            <Drawer isOpen={true} placement="left" onClose={() => { }}>
+                <DrawerOverlay>
+                    <DrawerContent bg='gray.800' p='4'>
+                        <DrawerCloseButton mt="6"/>
+                            <DrawerHeader>
+                                Navegação
+                            </DrawerHeader>
+                            <DrawerBody>
+                                <SideNavBar />
+                            </DrawerBody>
+                    </DrawerContent>
+                </DrawerOverlay>
+            </Drawer>
+        )
+    }
+
     return (
         <Box as="aside" w="64" mr="8">
-            <Stack spacing="12" align="flex-start">
-                <NavSection title="GERAL">
-                    <NavLink icon={RiDashboardLine}>
-                        Dashboard
-                    </NavLink>
-                    <NavLink icon={RiContactsLine}>
-                        Usuários
-                    </NavLink>
-                </NavSection>
-                <NavSection title="AUTOMAÇÂO">
-                    <NavLink icon={RiInputMethodLine}>
-                        Formulário
-                    </NavLink>
-                    <NavLink icon={RiContactsLine}>
-                        Automação
-                    </NavLink>
-                </NavSection>
-            </Stack >
+            <SideNavBar />
         </Box >
 
     )
