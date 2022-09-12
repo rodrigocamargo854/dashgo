d1da2af9e1bf516330619da35987bc66c208a383
Rodrigo Correa de Godoy Camargo <rodrigo.godoy@hbsis.com.br>
2022-09-12 06:52:04 -0300

    extract onOpen of context on Header

--- src/pages/components/Header/index.tsx
+++ src/pages/components/Header/index.tsx
@@ -1,10 +1,14 @@
-import { Flex, useBreakpointValue } from "@chakra-ui/react";
+import { Flex, IconButton, useBreakpointValue, Icon } from "@chakra-ui/react";
+import { RiMenuLine } from "react-icons/ri";
+import { useSidebarDrawer } from "../../context/SideBarContext";
 import { Logo } from "./Logo";
 
 import { NotificationsNav } from "./NotificationsNav";
 import { Profile } from "./Profile";
 import { SearchBox } from "./SearchBox";
 export function Header() {
+  const {onOpen  } = useSidebarDrawer()
+
   //hooks of chackra for  validations of responsivity
   const isWideVersion = useBreakpointValue({
     base: false,
@@ -24,7 +28,20 @@ export function Header() {
       p={0}
 
     >
+      {!isWideVersion && (
+        <IconButton
+          aria-label="Open Navigator"
+          icon={<Icon as={RiMenuLine} />}
+          fontSize="24"
+          variant="unstyled"
+          onClick={onOpen}
+          mr="2"
+        >
+        </IconButton>
+      )}
+
       <Logo />
+
       {isWideVersion && <SearchBox />}
       <Flex align="center" ml="auto">
         <NotificationsNav />
