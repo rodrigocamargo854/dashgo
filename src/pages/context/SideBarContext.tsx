ec5bd7f58a7dae0ee1310ec3fbbfcdb5dfcfd38c
Rodrigo Correa de Godoy Camargo <rodrigo.godoy@hbsis.com.br>
2022-09-12 06:51:01 -0300

    add useDisclosure in context on sideBarContext

--- src/pages/context/SideBarContext.tsx
+++ src/pages/context/SideBarContext.tsx
@@ -1,22 +1,22 @@
 import { useDisclosure, UseDisclosureReturn } from "@chakra-ui/react";
-import { createContext, ReactNode } from "react";
+import { createContext, ReactNode, useContext } from "react";
 
 
 type SidebarDrawerContexData = UseDisclosureReturn
-
 interface SideBarProviderProps {
     children: ReactNode
 }
 
-const SideBarDrawerContext = createContext({} as SidebarDrawerContexData )
-const disclosure = useDisclosure()
+const SideBarDrawerContext = createContext({} as SidebarDrawerContexData)
 
 export function SideBarDrawerProvider({ children }: SideBarProviderProps) {
+    const disclosure = useDisclosure()
     return (
         <SideBarDrawerContext.Provider value={disclosure}>
             {children}
         </SideBarDrawerContext.Provider>
     )
-
-
 }
+
+
+export const useSidebarDrawer = () => useContext(SideBarDrawerContext)
