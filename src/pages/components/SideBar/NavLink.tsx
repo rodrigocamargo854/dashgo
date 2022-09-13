6ce748ac7534b7805f8c7f4edc2c79b2fd2efad3
Rodrigo Correa de Godoy Camargo <rodrigo.godoy@hbsis.com.br>
2022-09-13 15:50:05 -0300

    insrt link for SideBar component

--- src/pages/components/SideBar/NavLink.tsx
+++ src/pages/components/SideBar/NavLink.tsx
@@ -1,16 +1,19 @@
-import { Icon, Link,Text, LinkProps as ChakraProps } from "@chakra-ui/react";
+import { Icon, Link as ChackraLink,Text, LinkProps as ChakraProps } from "@chakra-ui/react";
 import { ElementType } from "react";
-
+import Link from 'next/link'
 
 interface NavLinkProps extends ChakraProps {
     icon: ElementType;
     children: string
+    href: string
 }
-export function NavLink({icon,children,...rest}:NavLinkProps) {
+export function NavLink({icon,href,children,...rest}:NavLinkProps) {
     return (
-        <Link display="flex" alignItems='center' {...rest}>
+        <Link href={href} passHref>
+        <ChackraLink display="flex" alignItems='center' {...rest}>
             <Icon as={icon} fontSize="20" />
             <Text ml="4" fontWeight="medium">{children}</Text>
+        </ChackraLink>
         </Link>
     );
 }
