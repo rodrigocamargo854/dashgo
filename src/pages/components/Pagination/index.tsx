0fcfdebf7442d4cbdd4b68eef60fb8bd44bca22f
Rodrigo Correa de Godoy Camargo <rodrigo.godoy@hbsis.com.br>
2022-09-13 15:49:28 -0300

    responsivity for pagination component

--- src/pages/components/Pagination/index.tsx
+++ src/pages/components/Pagination/index.tsx
@@ -13,7 +13,7 @@ export function Pagination() {
             <Box>
                 <strong>0</strong>-<strong>10</strong> de <strong> 100</strong>
             </Box>
-            <Stack direction="row" spacing="2">
+            <Stack direction={["column", "row"]} spacing="2">
                 <PaginationItem number={1} isCurrent />
                 <PaginationItem number={2} />
                 <PaginationItem number={3} />
