72ad5a903903eec117ce099b14105fc80e9b810d
Rodrigo Correa de Godoy Camargo <rodrigo.godoy@hbsis.com.br>
2022-09-13 15:51:41 -0300

    added lin for users and index create

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -19,17 +19,17 @@ export default function CreateUser() {
             <Header />
             <Flex w="100%" my="6" maxWidth={1400} mx="auto" px="6">
                 <SideBar />
-                <Box flex="1" borderRadius="8" bg="gray.800" p="8">
+                <Box flex="1" borderRadius="8" bg="gray.800" p={["6", "8"]}>
                     <Heading size="lg" fontWeight="normal">Criar Usuário</Heading>
 
                     <Divider my="6" borderColor="gray.700" />
                     <VStack spacing="8">
-                        <SimpleGrid minChildWidth="248px" spacing="8" w="100%">
+                        <SimpleGrid minChildWidth="248px" spacing={["6", "8"]} w="100%">
                             <Input name="name" label="nome completo"></Input>
                             <Input name="email" type="email" label="E-mail"></Input>
                         </SimpleGrid>
 
-                        <SimpleGrid minChildWidth="248px" spacing="8" w="100%">
+                        <SimpleGrid minChildWidth="248px" spacing={["6","8"]} w="100%">
                             <Input name="password" label="password"></Input>
                             <Input name="password_confirmation" type=" password" label="confirmação da senha"></Input>
                         </SimpleGrid>
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -12,13 +12,20 @@ import {
     Tbody,
     Td,
     Text,
+    useBreakpointValue,
 } from "@chakra-ui/react";
 import { RiAddLine, RiPencilFill } from "react-icons/ri";
 import { Header } from "../components/Header/index";
 import { Pagination } from "../components/Pagination";
 import SideBar from "../components/SideBar";
-
+import Link from 'next/link';
 export default function UserList() {
+
+    const isWideVersion = useBreakpointValue({
+        base: false,
+        lg: true
+    })
+
     return (
         <Box>
             <Header />
@@ -29,40 +36,43 @@ export default function UserList() {
                         <Heading size="lg" fontWeight="normal">
                             Usuários
                         </Heading>
-                        <Button
-                            as="a"
-                            size="small"
-                            fontSize="small"
-                            colorScheme="pink"
-                            leftIcon={<Icon as={RiAddLine} />}
-                        >
-                            Criar Novo Usuário
-                        </Button>
+                        <Link href='/users/create' passHref>
+                            <Button
+                                as="a"
+                                size="small"
+                                fontSize="small"
+                                colorScheme="pink"
+                                leftIcon={<Icon as={RiAddLine} />}
+                            >
+                                Criar Novo Usuário
+                            </Button>
+
+                        </Link>
                     </Flex>
 
                     <Table colorScheme="whiteAlpha">
                         <Thead>
                             <Tr>
-                                <Th px="6" color="gray.300" width="8">
+                                <Th px={["4", "4", "6"]} color="gray.300" width="8">
                                     <Checkbox colorScheme="pink" />
                                 </Th>
                                 <Th>Usuários</Th>
-                                <Th>Data de cadastros</Th>
+                                {isWideVersion && <Th>Data de cadastros</Th>}
                                 <Th width="0"></Th>
                             </Tr>
                         </Thead>
                         <Tbody>
                             <Tr>
-                                <Td px="6">
+                                <Td px={["4", "4", "6"]}>
                                     <Checkbox colorScheme="pink" />
                                 </Td>
-                                <Td px="6">
+                                <Td px={["4", "4", "6"]}>
                                     <Box>
                                         <Text fontWeight="bold">Rodrigo Camargo</Text>
                                         <Text fontSize="sm" color="gray.300"  >rodrigocamargo854@gmail.com</Text>
                                     </Box>
                                 </Td>
-                                <Td>04 de abril, 2021</Td>
+                                {isWideVersion && <Td>04 de abril, 2021</Td>}
                                 <Td>
                                     <Button
                                         as="a"
@@ -77,7 +87,7 @@ export default function UserList() {
                             </Tr>
                         </Tbody>
                     </Table>
-                    <Pagination/>
+                    <Pagination />
                 </Box>
             </Flex>
         </Box>
