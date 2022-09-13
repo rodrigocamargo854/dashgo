e1b86acebacc87d4c6f37fa19b299c1526cb79d9
Rodrigo Correa de Godoy Camargo <rodrigo.godoy@hbsis.com.br>
2022-09-13 15:50:20 -0300

    insrt link for SideBar component

--- src/pages/components/SideBar/SideNavBar.tsx
+++ src/pages/components/SideBar/SideNavBar.tsx
@@ -7,18 +7,18 @@ export function SideNavBar() {
     return (
         <Stack spacing="12" align="flex-start">
             <NavSection title="GERAL">
-                <NavLink icon={RiDashboardLine}>
+                <NavLink icon={RiDashboardLine} href="/dashboard">
                     Dashboard
                 </NavLink>
-                <NavLink icon={RiContactsLine}>
+                <NavLink icon={RiContactsLine} href="/users">
                     Usuários
                 </NavLink>
             </NavSection>
             <NavSection title="AUTOMAÇÂO">
-                <NavLink icon={RiInputMethodLine}>
+                <NavLink icon={RiInputMethodLine} href="/forms">
                     Formulário
                 </NavLink>
-                <NavLink icon={RiContactsLine}>
+                <NavLink icon={RiContactsLine} href="/automation">
                     Automação
                 </NavLink>
             </NavSection>
