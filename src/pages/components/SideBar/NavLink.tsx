44de8041b0546376195890637a338af034606000
Rodrigo Correa de Godoy Camargo <rodrigo.godoy@hbsis.com.br>
2022-09-13 16:13:19 -0300

    created NavLink

--- src/pages/components/SideBar/NavLink.tsx
+++ src/pages/components/SideBar/NavLink.tsx
@@ -1,19 +1,20 @@
-import { Icon, Link as ChackraLink,Text, LinkProps as ChakraProps } from "@chakra-ui/react";
+import { Icon, Link as ChackraLink, Text, LinkProps as ChakraProps } from "@chakra-ui/react";
 import { ElementType } from "react";
 import Link from 'next/link'
+import { ActiveLink } from "../ActiveLink";
 
 interface NavLinkProps extends ChakraProps {
     icon: ElementType;
     children: string
     href: string
 }
-export function NavLink({icon,href,children,...rest}:NavLinkProps) {
+export function NavLink({ icon, href, children, ...rest }: NavLinkProps) {
     return (
-        <Link href={href} passHref>
-        <ChackraLink display="flex" alignItems='center' {...rest}>
-            <Icon as={icon} fontSize="20" />
-            <Text ml="4" fontWeight="medium">{children}</Text>
-        </ChackraLink>
-        </Link>
+        <ActiveLink href={href} passHref>
+            <ChackraLink display="flex" alignItems='center' {...rest}>
+                <Icon as={icon} fontSize="20" />
+                <Text ml="4" fontWeight="medium">{children}</Text>
+            </ChackraLink>
+        </ActiveLink>
     );
 }
