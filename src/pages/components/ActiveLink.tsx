f84d6fe34a1d668776b12940336393f6d43c20ec
Rodrigo Correa de Godoy Camargo <rodrigo.godoy@hbsis.com.br>
2022-09-14 06:17:12 -0300

    logic for active link finalized

--- src/pages/components/ActiveLink.tsx
+++ src/pages/components/ActiveLink.tsx
@@ -4,20 +4,28 @@ import { cloneElement, ReactElement } from "react"
 
 interface ActiveLinkProps extends LinkProps {
     children: ReactElement
+    shouldMatchExactref?: boolean
 }
 
-export function ActiveLink({ children, ...rest }: ActiveLinkProps) {
+export function ActiveLink({
+    shouldMatchExactref = false,
+    children, ...rest }: ActiveLinkProps
+) {
     const { asPath } = useRouter()
 
     let isActive = false
 
 
-    if (asPath === rest.href || asPath === rest.as) {
+    if (shouldMatchExactref && (asPath === rest.href || asPath === rest.as)) {
         isActive = true
     }
 
+    if (!shouldMatchExactref &&
+        (asPath.startsWith(String(rest.href)) ||
+            asPath.startsWith(String(rest.as)))) {
+        isActive = true
+    }
     return (
-
         <Link {...rest}>
             {cloneElement(children, {
                 color: isActive ? 'pink.400' : 'gray.400'
