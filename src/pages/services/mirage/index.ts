ab8d5f2e1436ca125307c9ecd815692a15902ea3
Rodrigo Camargo <rodrigocamargo854@gmail.com>
2022-10-01 06:44:47 -0300

    pagination get configured

--- src/pages/services/mirage/index.ts
+++ src/pages/services/mirage/index.ts
@@ -1,5 +1,5 @@
-import { createServer, Factory, Model } from "miragejs";
-import { faker } from '@faker-js/faker'
+import { createServer, Factory, Model, Response } from "miragejs";
+import { faker } from "@faker-js/faker";
 type user = {
   name: string;
   email: string;
@@ -13,20 +13,19 @@ export function makeServer() {
     },
     factories: {
       user: Factory.extend({
-        name(i:number) {
-          return `User ${i+1}`;
+        name(i: number) {
+          return `User ${i + 1}`;
         },
         email() {
           return faker.internet.email().toLowerCase();
         },
         createAt() {
-          return faker.date.recent(10,new Date());
+          return faker.date.recent(10, new Date());
         },
       }),
     },
     seeds(server) {
-      server.createList('user',10)
-
+      server.createList("user", 10);
     },
     routes() {
       //mocking api routing folder
@@ -34,7 +33,26 @@ export function makeServer() {
       //delay to testing loading
       this.timing = 750;
 
-      this.get("/users");
+      this.get("/users", function (schema, request) {
+        const { page = 1, per_page = 10 } = request.queryParams;
+
+        const total = schema.all("user").length;
+
+        const pageStart = (Number(page) - 1) * Number(per_page);
+        const pageEnd = pageStart + Number(per_page);
+
+        const users = this.serialize(schema.all("user")).users.slice(
+          pageStart,
+          pageEnd
+        );
+        return new Response(
+          200,
+          {
+            "x-total-count": String(total),
+          },
+          { users }
+        );
+      });
       this.post("/users");
       //reset routes
       this.namespace = "";
